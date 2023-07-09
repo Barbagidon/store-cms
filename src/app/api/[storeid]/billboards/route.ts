b6e71666f0ec0a5298d33dc4e1e822f97caad99e
Den Shtiller <77959708+Barbagidon@users.noreply.github.com>
2023-07-09 12:42:51 +0300

    feat: created route for billboard

---
+++ src/app/api/[storeid]/billboards/route.ts
@@ -0,0 +1,41 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export const POST = async (
+  req: Request,
+  { params }: { params: { storeId: string } }
+) => {
+  try {
+    const { userId } = auth();
+
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    if (!label) {
+      return new NextResponse("Label is required", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse("Image url is required", { status: 400 });
+    }
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const billboard = await prismadb.billboard.create({
+      data: {
+        label,
+        imageUrl,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[billboards_post]", error);
+    return new NextResponse("Inetrnal error", { status: 500 });
+  }
+};
