3d2ebc4e15baa831c595b951b83290bdb40e9979
Den Shtiller <77959708+Barbagidon@users.noreply.github.com>
2023-07-17 19:57:21 +0300

    feat: created size  page

---
+++ src/app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+
+import { Button } from '@/components/ui/button'
+import Heading from '@/components/ui/heading'
+import { Separator } from '@/components/ui/separator'
+import { Billboard } from '@prisma/client'
+import { Plus } from 'lucide-react'
+import { useParams, useRouter } from 'next/navigation'
+import React from 'react'
+import { SizeColumn, columns } from './columns'
+import { DataTable } from '@/components/ui/data-table'
+import ApiList from '@/components/ui/api-list'
+
+
+interface BillboardClientProps {
+    data: SizeColumn[]
+}
+
+const SizesClient = ({ data }: BillboardClientProps) => {
+
+    const router = useRouter()
+    const params = useParams()
+
+
+    return (
+        <>
+            <div className='flex items-center justify-between'>
+                <Heading description='Manage sizes for your store' title={`Sizes(${data.length})`} />
+                <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+                    <Plus className='mr-2 h-4 w-4' />
+                    Add new
+                </Button>
+            </div>
+            <Separator />
+            <DataTable searchKey='name' data={data} columns={columns} />
+            <Heading title='API' description='API calls for sizes' />
+            <Separator />
+            <ApiList entityName='sizes' entityIdName='sizeId' />
+
+        </>
+
+    )
+}
+
+export default SizesClient
