0233acffbe56da9b30f15afaf791d1cbbac0fa82
Den Shtiller <77959708+Barbagidon@users.noreply.github.com>
2023-07-21 16:16:42 +0300

    feat: created product client

---
+++ src/app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import ProductClient from './components/client'
+import prismadb from '@/lib/prismadb'
+import { ProductColumn } from './components/columns'
+import { format } from 'date-fns'
+import { formatter } from '@/lib/utils'
+
+
+const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
+
+    const products = await prismadb.product.findMany({
+        where: {
+            storeId: params.storeId
+        },
+        include: {
+            category: true,
+            size: true,
+            color: true,
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+
+
+
+
+
+
+
+    const formattedProducts: ProductColumn[] = products.map(({ id, name, createdAt, isFeatured, isArchived, price, category, color }) => ({
+        id,
+        name,
+        isFeatured,
+        isArchived,
+        category: category.name,
+        size: category.name,
+        color: color.value,
+        price: formatter.format(price.toNumber()),
+        createdAt: format(createdAt, 'MMMM do, yyyy')
+    }))
+
+
+
+    return (
+        <div className='flex-col'>
+            <div className='flex-1 space-y-4 p-8 pt-6'>
+                <ProductClient data={formattedProducts} />
+            </div>
+        </div>
+    )
+}
+
+export default ProductsPage
